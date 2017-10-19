341bf70a32ddc1fbde5a66a2d90c0259afaf3dd8
Alexander Balyshyn <balyszyn@gmail.com>
2017-10-19 21:42:14 +0300

    git push -u origin master Initial commit 	new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/omniesoft/commerce/statistic/omniestatistic/OmnieStatisticApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/omniesoft/commerce/statistic/omniestatistic/OmnieStatisticApplicationTests.java

---
+++ src/main/java/com/omniesoft/commerce/statistic/omniestatistic/OmnieStatisticApplication.java
@@ -0,0 +1,12 @@
+package com.omniesoft.commerce.statistic.omniestatistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnieStatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnieStatisticApplication.class, args);
+	}
+}
