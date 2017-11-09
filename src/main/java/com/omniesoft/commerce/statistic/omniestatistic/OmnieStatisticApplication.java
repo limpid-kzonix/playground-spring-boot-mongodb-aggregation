47930f9d8a1ff9e58eed6aa26ffa87651b3d4b8d
Alexander Balyshyn <balyszyn@gmail.com>
2017-11-09 17:49:57 +0200

    add validator for mongo document classes

--- src/main/java/com/omniesoft/commerce/statistic/omniestatistic/OmnieStatisticApplication.java
+++ src/main/java/com/omniesoft/commerce/statistic/omniestatistic/OmnieStatisticApplication.java
@@ -2,10 +2,7 @@ package com.omniesoft.commerce.statistic.omniestatistic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -15,13 +12,4 @@ public class OmnieStatisticApplication {
 		SpringApplication.run(OmnieStatisticApplication.class, args);
 	}
 
-	@Bean
-	public ValidatingMongoEventListener validatingMongoEventListener() {
-		return new ValidatingMongoEventListener(validator());
-	}
-
-	@Bean
-	public LocalValidatorFactoryBean validator() {
-		return new LocalValidatorFactoryBean();
-	}
 }
