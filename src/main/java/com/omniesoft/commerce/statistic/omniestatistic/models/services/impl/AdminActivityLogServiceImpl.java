2aaa883c453ff246bec924694e94ac4373a6111c
Alexander Balyshyn <balyszyn@gmail.com>
2017-11-15 17:54:52 +0200

    implement security for communication between services.  add rest service for exchange token for inter-service communication  test rest service for statistic

--- src/main/java/com/omniesoft/commerce/statistic/omniestatistic/models/services/impl/AdminActivityLogServiceImpl.java
+++ src/main/java/com/omniesoft/commerce/statistic/omniestatistic/models/services/impl/AdminActivityLogServiceImpl.java
@@ -1,7 +1,6 @@
 package com.omniesoft.commerce.statistic.omniestatistic.models.services.impl;
 
 import com.omniesoft.commerce.common.ws.statistic.impl.payload.AdminLogPayload;
-import com.omniesoft.commerce.statistic.omniestatistic.models.entities.AdminActivityLogEntity;
 import com.omniesoft.commerce.statistic.omniestatistic.models.enums.AdminActionType;
 import com.omniesoft.commerce.statistic.omniestatistic.models.repositories.AdminActivityLogRepository;
 import com.omniesoft.commerce.statistic.omniestatistic.models.services.AdminActivityLogService;
@@ -17,12 +16,6 @@ public class AdminActivityLogServiceImpl implements AdminActivityLogService {
 	@Override
 	public void insert(AdminLogPayload logPayload, AdminActionType type) {
 
-		AdminActivityLogEntity adminActivityLogEntity = new AdminActivityLogEntity();
-		adminActivityLogEntity.setType(type);
-		adminActivityLogEntity.setOrganizationId(logPayload.getOrganizationId().toString());
-		adminActivityLogEntity.setAdminId(logPayload.getAdminId().toString());
-		adminActivityLogEntity.setDateTime(logPayload.getDateTime());
 
-		adminActivityLogRepository.insert(adminActivityLogEntity);
 	}
 }
